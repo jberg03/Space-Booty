caa299d75923ebeb2ffb507a3ff15d4ba10b83df
John Berg <jdberg18@hotmail.com>
2015-12-27 22:34:11 -0500

    initial commit

---
+++ desktop/src/com/johnberg/spacebooty/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.johnberg.spacebooty.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.johnberg.spacebooty.SpaceBooty;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceBooty(), config);
+	}
+}
