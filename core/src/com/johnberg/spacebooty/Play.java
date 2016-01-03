9ccf31952e0e9957e73bc6849cc7ca5da10b7a13
John Berg <jdberg18@hotmail.com>
2016-01-03 16:25:17 -0500

    Got the Hideout on the screen

---
+++ core/src/com/johnberg/spacebooty/Play.java
@@ -0,0 +1,88 @@
+package com.johnberg.spacebooty;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * Space-Booty
+ * Created by john on 12/31/15.
+ */
+public class Play implements Screen {
+    private Batch batch;
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer renderer;
+    private TiledMapTileLayer gameLayer;
+    private TiledMapTileLayer markerLayer;
+    private TiledMap tiledMap;
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.setToOrtho(false, 23, 15);
+
+        batch.setProjectionMatrix(camera.combined);
+        renderer.setView(camera);
+
+        renderer.getSpriteBatch().begin();
+        renderer.renderTileLayer(gameLayer);
+        renderer.getSpriteBatch().end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+
+        tiledMap = new TmxMapLoader().load("fortress.tmx");
+
+        //Create the Game Layer
+        gameLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Game");
+
+        //Create the Marker layer
+        markerLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Marker");
+        int column = markerLayer.getWidth();
+        int row = markerLayer.getHeight();
+        TiledMapTileLayer.Cell cell = markerLayer.getCell(column, row);
+
+        float unitScale = 1f / 48f;
+        renderer = new OrthogonalTiledMapRenderer(tiledMap, unitScale);
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+        batch.dispose();
+        renderer.dispose();
+    }
+}
--- desktop/src/com/johnberg/spacebooty/desktop/DesktopLauncher.java
+++ desktop/src/com/johnberg/spacebooty/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.johnberg.spacebooty.SpaceBooty;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1080;
+		config.height = 720;
+        config.title = SpaceBooty.TITLE;
 		new LwjglApplication(new SpaceBooty(), config);
 	}
 }
